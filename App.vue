ffd4f475fd192caab94c7e841811f8e42552d7c6
lyc <405624843@qq.com>
2020-12-15 17:17:04 +0800

    全部项目

---
+++ App.vue
@@ -0,0 +1,210 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/*每个页面公共css */
+	.flex {
+		display: flex;
+		flex-direction: row;
+	}
+	
+	.flex-between {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	
+	.flex-center {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+	
+	.flex-around {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+	}
+	
+	.flex-between {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	
+	.bold {
+		font-weight: bold;
+	}
+	
+	.iconfont {
+		font-size: 28rpx;
+	}
+	
+	.fs44 {
+		font-size: 44rpx;
+	}
+	
+	.fs40 {
+		font-size: 40rpx;
+	}
+	
+	.fs38 {
+		font-size: 38rpx;
+	}
+	
+	.fs36 {
+		font-size: 36rpx;
+	}
+	
+	.fs34 {
+		font-size: 34rpx;
+	}
+	
+	.fs32 {
+		font-size: 32rpx;
+	}
+	
+	.fs30 {
+		font-size: 30rpx;
+	}
+	
+	.fs26 {
+		font-size: 26rpx;
+	}
+	.fs24 {
+		font-size: 24rpx;
+	}
+	.fs22 {
+		font-size: 22rpx;
+	}
+	
+	.ptb24 {
+		padding: 24rpx 0;
+	}
+	
+	.ptb14 {
+		padding: 14rpx 0;
+	}
+	
+	.plf24 {
+		padding: 0 24rpx;
+	}
+	
+	.plf14 {
+		padding: 0 14rpx;
+	}
+	.pa14 {
+		padding: 14rpx;
+	}
+	
+	.pa24 {
+		padding: 24rpx;
+	}
+	
+	.pa6-22 {
+		padding: 6rpx 22rpx;
+	}
+	
+	.par14 {
+		padding-right: 14rpx;
+	}
+	
+	.mtb24 {
+		margin: 24rpx 0;
+	}
+	
+	.mlf24 {
+		margin: 0 24rpx;
+	}
+	
+	.mlf14 {
+		margin: 0 14rpx;
+	}
+	
+	.ma24 {
+		margin: 24rpx;
+	}
+	
+	.ma14 {
+		margin: 14rpx;
+	}
+	.ma8 {
+		margin: 8rpx;
+	}
+	
+	.mtb14 {
+		margin: 14rpx 0;
+	}
+	
+	.lh52 {
+		line-height: 52rpx;
+	}
+	
+	.lh48 {
+		line-height: 48rpx;
+	}
+	
+	.lh46 {
+		line-height: 46rpx;
+	}
+	
+	.lh42 {
+		line-height: 42rpx;
+	}
+	
+	.bg-ffffff {
+		background: #FFFFFF;
+	}
+	
+	.bg-f5f5f5 {
+		background-color: #F5F5F5;
+	}
+	
+	.bg-efefef {
+		background-color: #efefef;
+	}
+	
+	.bg-fafafa {
+		background-color: #fafafa;
+	}
+	.bg-00AA00{
+		background-color: #00AA00;
+	}
+	
+	.border-radius {
+		border-radius: 10rpx;
+		overflow: hidden;
+	}
+	
+	.col90 {
+		color: #909090;
+	}
+	
+	.col64 {
+		color: #646464;
+	}
+	
+	.col333 {
+		color: #333333;
+	}
+	
+	.col666 {
+		color: #666666;
+	}
+	
+	.col999 {
+		color: #999999;
+	}
+</style>
